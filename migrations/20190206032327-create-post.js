1f4606914887d48eab33082398bed200cbe5a14a
Taralite <taralite@Taralites-MacBook-Air.local>
2019-02-06 17:08:47 +0700

    updated migrations psot

--- migrations/20190206032327-create-post.js
+++ migrations/20190206032327-create-post.js
@@ -17,6 +17,9 @@ module.exports = {
       tags: {
         type: Sequelize.ARRAY(Sequelize.STRING)
       },
+      userId : {
+        type: Sequelize.INTEGER
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE

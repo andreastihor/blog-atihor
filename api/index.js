9c003e50418c834ccf52e9310bfa74d328deb0c6
Taralite <taralite@Taralites-MacBook-Air.local>
2019-02-06 17:08:29 +0700

    route added

--- api/index.js
+++ api/index.js
@@ -3,17 +3,17 @@ const post = require('./handlers/post');
 
 module.exports = {
   register : (server) => {
-  server.route([
+    server.route([
+      {method : 'GET' , path : '/' , options: post.getPost},
+      // {method : 'GET' , path : '/{username}/{tags}/{post}/get' , options: post.searchPost },
+      {method : 'POST' , path : '/signin' , options: user.login  },
+      {method : 'POST' , path : '/createPost' , options: post.createPost },
+      // {method : 'GET' , path : '/{id}/update' , options: post.getUpdatePost  },
+      {method : 'PUT' , path : '/{id}/updatePost' , options: post.updatePost  },
+      {method : 'DELETE' , path : '/{id}/deletePost' , options: post.deletePost  },
 
-    // {method : 'GET' , path : '/{username}/post/get' , options: post.getPost },
-    // {method : 'GET' , path : '/{username}/{tags}/{post}/get' , options: post.searchPost },
-    {method : 'POST' , path : '/signin' , options: user.login  },
-    {method : 'POST' , path : '/createPost' , options: post.createPost },
-    // {method : 'PUT' , path : '/{post}/update' , options: post.updatePost  },
-    // {method : 'DELETE' , path : '/{post}/delete' , options: post.deletePost  },
 
-
-  ])
+    ])
 },
 name : 'api-plugin'
 }

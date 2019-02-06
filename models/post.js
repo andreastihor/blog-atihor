a1a74e7e9c2c8c13859243bc8e57ff026601e539
Taralite <taralite@Taralites-MacBook-Air.local>
2019-02-06 17:07:06 +0700

    model updated

--- models/post.js
+++ models/post.js
@@ -3,6 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   const post = sequelize.define('post', {
     title: DataTypes.STRING,
     content: DataTypes.STRING,
+    userId : DataTypes.INTEGER,
     tags: DataTypes.ARRAY(DataTypes.STRING)
   }, {});
   post.associate = function(models) {

6cae0950d07c1532312762f433e82f416e735486
Taralite <taralite@Taralites-MacBook-Air.local>
2019-02-07 17:30:23 +0700

    all route completed

--- api/index.js
+++ api/index.js
@@ -5,10 +5,9 @@ module.exports = {
   register : (server) => {
     server.route([
       {method : 'GET' , path : '/' , options: post.getPost},
-      // {method : 'GET' , path : '/{username}/{tags}/{post}/get' , options: post.searchPost },
+      {method : 'GET' , path : '/search/{tags}' , options: post.searchPost },
       {method : 'POST' , path : '/signin' , options: user.login  },
       {method : 'POST' , path : '/createPost' , options: post.createPost },
-      // {method : 'GET' , path : '/{id}/update' , options: post.getUpdatePost  },
       {method : 'PUT' , path : '/{id}/updatePost' , options: post.updatePost  },
       {method : 'DELETE' , path : '/{id}/deletePost' , options: post.deletePost  },
 
